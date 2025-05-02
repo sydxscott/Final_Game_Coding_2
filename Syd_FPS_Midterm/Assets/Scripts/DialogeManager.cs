fb006e0ce0f7c61183524235ec9a5ac33347a25a
sydxscott <sscott17@pratt.edu>
2025-05-01 20:44:41 -0400

    End of Class 5/1: Started on Dialoge Manager

--- Syd_FPS_Midterm/Assets/Scripts/DialogeManager.cs
+++ Syd_FPS_Midterm/Assets/Scripts/DialogeManager.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogeManager : MonoBehaviour
 {
-    public TextMeshProUGUI Dialoge;
-    public GameObject textBox;
+    public TextMeshProUGUI dialoge;
+    public Image textBox;
 
-    
+    public string[] dialogeLines;
+
+    private void Start()
+    {
+       
+        
+    }
+
     public void LevelOneZombiesDead()
     {
+        dialoge.gameObject.SetActive(true);
+        textBox.gameObject.SetActive(true);
+
+        dialoge.text = "I think all the uggo zombies are gone!";
+
+
 
     }
+
+    //create funstions taht can be called in different scripts when different things happen 
+    // each of these functiosn has an array for dialoge line that it cycles throgh 
+    // use seth code 
 
 
 }
