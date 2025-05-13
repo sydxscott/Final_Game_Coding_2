122b46b2f5f64c4d6a5153cf36161bda5e3aa6d3
sydxscott <sscott17@pratt.edu>
2025-05-13 12:10:29 -0400

    mari fix animation and inverted camera

--- Syd_FPS_Midterm/Assets/Scripts/AimStateManager.cs
+++ Syd_FPS_Midterm/Assets/Scripts/AimStateManager.cs
@@ -24,8 +24,8 @@ public class AimStateManager : MonoBehaviour
 
     private void LateUpdate()
     {
-       camFollowPos.localEulerAngles = new Vector3(yAxis.Value, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
-       transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
+       camFollowPos.localEulerAngles = new Vector3(-yAxis.Value, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
+       transform.eulerAngles = new Vector3(-transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
     }
 
 }
